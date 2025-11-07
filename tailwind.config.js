f9bc837c1515b30cd408ab7898a0c5afb2121dc2
Othmane haiba <othmanehaiba4@gmail.com>
2025-11-07 16:28:26 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html", // Scans all HTML files in your project directory and subdirectories
+    // Add other paths here if you use JS or other template languages:
+    // "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
