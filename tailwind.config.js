31de0f0347bd4955dbd0c8081eb5c2aad05a8576
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2025-11-08 23:18:27 +0100

    create the hero section and started on the about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.html", // Scans all HTML files in your project directory and subdirectories
-    // Add other paths here if you use JS or other template languages:
-    // "./src/**/*.{js,jsx,ts,tsx}",
+    "./*.html",          // all HTML files in the root
+    "./**/*.html",       // all HTML files in subfolders
+    // if you later add JS/TS files, you can include:
+    // "./**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oleo: ['Oleo Script', 'cursive'],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        prixColor: '#007A4B',
+        bodyColor: '#FFEEB1',
+        headerYellow: "#f3c623",
+        headerOrange: "#f39223",
+      },
+    },
   },
   plugins: [],
 }
-
