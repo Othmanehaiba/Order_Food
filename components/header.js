c0405a67a8c3557536d52fc8f00298d355c84c3c
hamzalafsioui <hamzalafsioui@gmail.com>
2025-11-08 21:43:25 +0100

    add initHeaderMenu()

--- components/header.js
+++ components/header.js
@@ -77,5 +77,13 @@ export function Header() {
     </div>
 
 `;
-
+}
+export function initHeaderMenu() {
+  const menuBtn = document.getElementById("menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+  if (menuBtn && mobileMenu) {
+    menuBtn.addEventListener("click", () =>
+      mobileMenu.classList.toggle("hidden")
+    );
+  }
 }
