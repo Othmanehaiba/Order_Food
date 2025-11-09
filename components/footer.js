240c38f82e97caf406a13b776b2bab537888b834
hamzalafsioui <hamzalafsioui@gmail.com>
2025-11-09 22:22:35 +0100

    implement toogleMenuFooter js

--- components/footer.js
+++ components/footer.js
@@ -25,7 +25,7 @@ export function Footer() {
         <div>
           <button
             class="w-full flex justify-between items-center text-lg font-bold focus:outline-none"
-            onclick = toogleMenuFooter("company-menu");
+            onclick = "toogleMenuFooter('company-menu')"
           >
             Company
              <span id="company-arrow">+</span>
@@ -45,7 +45,7 @@ export function Footer() {
         <div>
           <button
             class="w-full flex justify-between items-center text-lg font-bold focus:outline-none"
-            onclick = toogleMenuFooter("info-menu");
+            onclick = "toogleMenuFooter('info-menu')"
           >
             Our Information
              <span id="info-arrow">+</span>
@@ -64,7 +64,7 @@ export function Footer() {
         <div>
           <button
             class="w-full flex justify-between items-center text-lg font-bold focus:outline-none"
-            onclick = toogleMenuFooter("contact-menu");
+            onclick = "toogleMenuFooter('contact-menu')"
           >
             Contact Info
              <span id="contact-arrow">+</span>
@@ -138,6 +138,20 @@ export function Footer() {
 export function toogleMenuFooter(id){
     const menu = document.getElementById(id);
     const arrow = document.getElementById(id.replace("menu","arrow"));
-    const idHidden = menu.classList.contains("hidden");
+    const isHidden = menu.classList.contains("hidden");
+
+    // close all menu [id$='menu'] get all id end with menu
+    document.querySelectorAll("[id$='menu']").forEach((el)=>{
+        el.classList.add("hidden");
+    });
+    document.querySelectorAll("[id$='arrow']").forEach((el)=>{
+        el.textContent = "+"
+    });
+
+    // toggle selected menu
+    if(isHidden){
+        menu.classList.remove("hidden");
+        arrow.textContent = '-';
+    }
 
 }
