976714e7e657fa3f34dfa6585913026ae2efc879
hamzalafsioui <hamzalafsioui@gmail.com>
2025-11-10 17:02:05 +0100

    add review js

--- js/details.js
+++ js/details.js
@@ -1,23 +1,18 @@
-const reviews =
-  JSON.parse(JSON.stringify("../data/customerReviews.json")) || [];
-const currentReview = reviews[0];
+let reviews = [];
+let currentReview;
 
-const authorEl = document.querySelector("author-el");
-const speechEl = document.querySelector("speech-el");
-const starsContainer = document.querySelector("stars-container");
-const profileImages = document.querySelectorAll("profile-images");
+const authorEl = document.querySelector(".author-el");
+const speechEl = document.querySelector(".speech-el");
+const starsContainer = document.querySelector(".stars-container");
+const profileImages = document.querySelectorAll(".profile-images img");
 
 function renderStars(starCount) {
   starsContainer.innerHTML = "";
   for (let i = 0; i < 5; i++) {
     const span = document.createElement("span");
     span.classList.add("rounded-3xl");
-    if (i < starCount) {
-      span.innerHTML = `<img src="../assets/stars.svg" alt="star" />`;
-      span.classList.add("bg-headerYellow");
-    } else {
-      span.innerHTML = `<img src="../assets/stars-gray.svg" alt="star" />`;
-    }
+    span.innerHTML = `<img src="../assets/stars.svg" alt="star" />`;
+    if (i < starCount) span.classList.add("bg-headerYellow");
     starsContainer.appendChild(span);
   }
 }
@@ -27,7 +22,6 @@ function updateReview(review) {
   speechEl.textContent = `“${review.speech}”`;
   renderStars(review.star);
 
-  // Highlight active image 
   profileImages.forEach((img) => {
     if (img.getAttribute("src") === review.imgUrl) {
       img.classList.add("bg-yellow-400", "py-5");
@@ -36,15 +30,28 @@ function updateReview(review) {
     }
   });
 }
-// event to profile images  
-profileImages.forEach((img) => {
-  img.addEventListener("click", () => {
-    const selected = reviews.find(
-      (r) => r.imgUrl === img.getAttribute("src")
-    );
-    if (selected) {
-      currentReview = selected;
-      updateReview(selected);
-    }
-  });
-});
+
+async function loadReviews() {
+  try {
+    const res = await fetch("../data/reviews.json");
+    reviews = await res.json();
+    currentReview = reviews[0];
+    updateReview(currentReview);
+
+    profileImages.forEach((img) => {
+      img.addEventListener("click", () => {
+        const selected = reviews.find(
+          (r) => r.imgUrl === img.getAttribute("src")
+        );
+        if (selected) {
+          currentReview = selected;
+          updateReview(selected);
+        }
+      });
+    });
+  } catch (err) {
+    console.error("Error loading reviews:", err);
+  }
+}
+
+loadReviews();
