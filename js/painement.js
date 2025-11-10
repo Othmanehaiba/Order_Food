a5791d5799dffdce94af8c6b606ead496805eb45
maryemlakhouil <lkhwilmaryem@gmail.com>
2025-11-10 17:18:33 +0100

    commancer la partie js

---
+++ js/painement.js
@@ -0,0 +1,11 @@
+// Declaration Des variables 
+
+ const Formulaire = {
+    
+     name : document.getElementById("name"),
+     phone : document.getElementById("phone"),
+     email : document.getElementById("email"),
+     adress : document.getElementById("adress"),
+     terms : document.getElementById("terms"),
+};
+const bytbtn = document.getElementById("pay-btn");
