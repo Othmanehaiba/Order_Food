6cd0e4bf8bebb4c2c7ab1523a76b9dc12088ad9b
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2025-11-12 09:46:56 +0100

    Adding a grid for showing dishes

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// const dots = document.querySelectorAll("#carousel-dots .dot");
+
+// // Example: activate the second dot
+// dots.forEach(dot => dot.classList.remove("bg-gray-800")); // remove active class
+// dots[1].classList.add("bg-gray-800"); // set active dot
