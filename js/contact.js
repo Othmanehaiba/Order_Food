95e8f3ee5f80fb876540fd82bfc6ea122d3cdceb
Othmane haiba <othmanehaiba4@gmail.com>
2025-11-13 17:05:15 +0100

    the final version html it only needs responsive

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,13 @@
 const qst = document.querySelectorAll(".qst-js");
-const ans = document.querySelector(".answer");
+
 qst.forEach((f) => {
-  f.addEventListener("click", () => {
-    ans.style.display = 'block'
-    
+  f.addEventListener('mouseover', () => {
+    const ans = f.nextElementSibling;
+    ans.style.display = 'block';
+  });
+  f.addEventListener('mouseout', () => {
+    const ans = f.nextElementSibling;
+    ans.style.display = 'none';
   });
 
 });
