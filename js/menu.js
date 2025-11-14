3e99a8bd2ecb1f5ace50d4bd1a6f9e6f672f6861
ayouberrak <ayouberrak30@gmail.com>
2025-11-14 15:02:06 +0100

    Refactor menu.js: Improve product display and add local storage functionality for panier

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,3 @@
-// le select de mobile
 document.addEventListener("DOMContentLoaded", () => {
   const customSelect = document.querySelector(".block.md\\:hidden");
 
@@ -60,8 +59,6 @@ document.addEventListener("DOMContentLoaded", () => {
         optionsList.classList.add("hidden");
         chevronIcon.classList.remove("rotate-180");
 
-        console.log("Selected category now:", currentTriggerData.categorie);
-
         e.stopPropagation();
       });
     });
@@ -72,8 +69,6 @@ document.addEventListener("DOMContentLoaded", () => {
         chevronIcon.classList.remove("rotate-180");
       }
     });
-  } else {
-    console.error("Ma l9itch l-element dial custom select.");
   }
 });
 
@@ -86,12 +81,17 @@ let currentData = [];
 let cuurentPage = 1;
 let cardInPage = 6;
 
+// function saveLOcalstorage(card){
+//   localStorage.setItem('card', JSON.stringify(card));
+// }
+
 fetch("../data/data.json")
   .then((res) => res.json())
   .then((data) => {
     allData = data;
     currentData = data;
     sendData();
+    // saveLOcalstorage(data);
   })
   .catch((err) => console.error("Erreur de serveur: " + err));
 
@@ -106,30 +106,204 @@ function sendData() {
 
   CardOn.forEach((e) => {
     menuDiv.innerHTML += `
-      <a href="details.html?id=${e.id}" class="block bg-white rounded-[1.25rem] shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div href="details.html?id=${e.id}" class="block bg-white rounded-[1.25rem] shadow-lg hover:shadow-xl transition-shadow duration-300">
       <div data-id=${e.id} class="w-full h-[29rem] p-[1.5rem] bg-headerYellow rounded-[20px] shadow-[0_4px_20px_rgba(255,122,0,0.7)]">
-        <div class="w-full h-[75%] md:h-[75%]">
-          <img src="${e.image}" alt="image-test" class="w-full h-full rounded-[20px]">
-        </div>
+        <a href="details.html?id=${e.id}">
+          <div class="w-full h-[75%] md:h-[75%] overflow-hidden rounded-[20px]">
+            <img src="${e.images[0]}" alt="image-test" class="w-full h-full rounded-[20px] hover:scale-125 transition-transform duration-300 object-cover">
+          </div>
+        </a>
         <div>
           <h3 class="font-roboto font-bold text-[1.45rem] ml-[1.5rem] mt-[1rem]">${e.name}</h3>
           <div class="flex items-center justify-center gap-[7rem] md:gap-[5rem] mt-[0.3rem]">
-            <span class="font-oleo font-bold text-prixColor text-[2rem] ml-[1rem]">${e.price}$</span>
-            <button class="bg-prixColor w-[9rem] h-[3rem] text-[1.1rem] font-bold font-roboto text-white rounded-[50px]">
+            <span class="font-oleo font-bold text-prixColor text-[2rem] ml-[1rem]">${e.basePrice}$</span>
+            
+            <button data-id=${e.id} class="btn-add-panier bg-prixColor w-[9rem] h-[3rem] text-[1.1rem] font-bold font-roboto text-white rounded-[50px]">
               Add to panier
             </button>
+          
           </div>
         </div>
       </div>
-    </a>
+    </div>
     `;
   });
 
+  // locale storage de panier
+  function addPanierToLocal(id) {
+    const selectedCard = allData.find((card) => card.id == id);
+    if (selectedCard) {
+      let panierData = JSON.parse(localStorage.getItem("panier")) || [];
+      panierData.push({ ...selectedCard, quantity: 1 });
+      localStorage.setItem("panier", JSON.stringify(panierData));
+      console.log("success");
+      
+      // icon success
+      Swal.fire({
+        icon: "success",
+        title: "Added to Panier!",
+        text: `${selectedCard.name} has been added.`,
+        showCancelButton: false,
+        confirmButtonText: "OK",
+        cancelButtonText: "Continue Shopping",
+        confirmButtonColor: "#f3c623",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // window.location.href = "../pages/menu.html";
+        }
+      });
+    }
+  }
+
+  // gestion des add to panier
+  let addpaniers = document.querySelectorAll(".btn-add-panier");
+  function isProductInPanier(panierData, id) {
+    return panierData.some((item) => item.id == id);
+  }
+
+  addpaniers.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if (
+        isProductInPanier(
+          JSON.parse(localStorage.getItem("panier")) || [],
+          e.currentTarget.dataset.id
+        )
+      ) {
+        Swal.fire({
+          icon: "error",
+          title: "Failed!",
+          text: `This plat is already in your panier.
+          You can increase the quantity from the panier.`,
+          confirmButtonText: "OK",
+          confirmButtonColor: "#e74c3c",
+        });
+        console.log("product exist");
+
+        return;
+      }
+
+      const id = e.currentTarget.dataset.id;
+      const selectedCard = allData.find((card) => card.id == id);
+
+      if (selectedCard) {
+        addPanierToLocal(id);
+        // add card for local to panier
+
+        function addPanier() {
+          let panierElement = document.getElementById("panier-cards");
+          const dataa = localStorage.getItem("panier");
+          const panierItems = dataa ? JSON.parse(dataa) : [];
+
+          panierElement.innerHTML = "";
+          panierItems.forEach((item) => {
+            panierElement.innerHTML += `
+        <div class="flex items-start justify-between border-t border-black/40  mr-[1rem] ml-[1rem]  pb-4">
+                <!-- Bloc gauche -->
+                <div class="flex items-start gap-4">
+
+                  <!-- Badge quantité -->
+                  <div class="w-12 h-12 bg-orange-500 rounded-full flex items-center justify-center mt-[1rem]">
+                    <span class="text-white font-bold text-lg">${item.quantity}x</span>
+                  </div>
+
+                  <!-- Infos commande -->
+                  <div class="space-y-1">
+                    <p class="text-prixColor font-semibold text-xl font-oleo">$${item.basePrice}</p>
+                    <p class="font-bold text-black font-roboto">${item.name}</p>
+                    <p class="text-black text-sm opacity-80 leading-4">
+                      No Mushrooms + green peppers
+                    </p>
+                  </div>
+                </div>
+
+                <!-- Bouton suppression -->
+                <button class="opacity-70 hover:opacity-100 mt-[2rem] delete-item " data-id="${item.id}">
+                  <img src="../assets/logo corbeille.png" class="w-7">
+                </button>
+
+              </div>`;
+          });
+          deleteCard();
+          afficherPrice();
+        }
+        addPanier();
+
+        function totalAfterDelete() {
+          afficherPrice();
+        }
+
+        function deleteCard() {
+          let deleteCards = document.getElementsByClassName("delete-item");
+          deleteCards = Array.from(deleteCards);
+          deleteCards.forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+              const idToDelete = e.currentTarget.dataset.id;
+
+              let panierData = JSON.parse(localStorage.getItem("panier")) || [];
+              panierData = panierData.filter((item) => item.id != idToDelete);
+              localStorage.setItem("panier", JSON.stringify(panierData));
+
+              totalAfterDelete();
+
+              const cardToDelete = e.currentTarget.parentElement;
+              cardToDelete.remove();
+            });
+          });
+        }
+
+        function afficherPrice() {
+          const dataa = localStorage.getItem("panier");
+          const panierItems = dataa ? JSON.parse(dataa) : [];
+
+          const total1 = document.getElementById("totalPrice");
+          const total2 = document.getElementById("totalPrice2");
+
+          let total = 0;
+          if (panierItems.length === 0) {
+            total1.innerHTML = `          
+          <h2 class="text-[1.5rem] font-bold font-roboto text-white">Total to pay</h2>
+          <span class="font-roboto font-bold text-[1.5rem] text-white">${total}</span>`;
+            total2.innerHTML = `          
+         <h2 class="font-roboto font-bold text-[1.5rem]">Sub Total: </h2>
+            <span class="font-roboto font-bold text-[1.5rem] text-prixColor">${total}</span>`;
+            return;
+          }
+
+          panierItems.forEach((item) => {
+            total += parseFloat(item.basePrice);
+          });
+          total += 5;
+          total = total.toFixed(2);
+
+          total1.innerHTML = `          
+        <h2 class="text-[1.5rem] font-bold font-roboto text-white">Total to pay</h2>
+        <span class="font-roboto font-bold text-[1.5rem] text-white">${total}</span>`;
+
+          total2.innerHTML = `          
+       <h2 class="font-roboto font-bold text-[1.5rem]">Sub Total: </h2>
+          <span class="font-roboto font-bold text-[1.5rem] text-prixColor">${total}</span>`;
+        }
+      }
+    });
+  });
+  // gestion des buttons de pagination
   if (cuurentPage === 1) {
     prevnBtn.classList.add("none");
   }
   nextBtn.disabled = end >= currentData.length;
 }
+
+let nextBtnMobile = document.getElementById("next-page-mobile");
+let prevnBtnMobile = document.getElementById("prev-page-mobile");
+nextBtnMobile.addEventListener("click", () => {
+  cuurentPage++;
+  sendData();
+});
+
+prevnBtnMobile.addEventListener("click", () => {
+  cuurentPage++;
+  sendData();
+});
 
 nextBtn.addEventListener("click", () => {
   cuurentPage++;
@@ -147,6 +321,16 @@ const filtre = document.querySelectorAll("[data-categorie]");
 filtre.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const category = e.currentTarget.dataset.categorie;
+    document.querySelectorAll("[data-categorie]").forEach((button) => {
+      button.classList.remove("shadow-[6px_6px_15px_rgba(0,0,0,0.25)]");
+    });
+
+    e.currentTarget.classList.add(
+      "shadow-[6px_6px_15px_rgba(0,0,0,0.25)]",
+      "p-6",
+      "rounded-xl",
+      "duration-300"
+    );
 
     let filteredData;
     switch (category) {
@@ -183,3 +367,11 @@ searchInput.addEventListener("input", (e) => {
   cuurentPage = 1;
   sendData();
 });
+
+function savetoPanier(card) {
+  let panier = JSON.parse(localStorage.getItem("panier")) || [];
+  panier.push(card);
+  localStorage.setItem("panier", JSON.stringify(panier));
+}
+
+console.log(total);
